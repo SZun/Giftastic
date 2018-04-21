085a1dbe9d0281ee9a39afac3f081874c7a784a4
Sam G. Zun <zunsamuel97@gmail.com>
2018-04-21 04:20:32 -0500

    commit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,21 +1,67 @@
-    
-var topics = ['christmas','navidad','santa','christmas+tree','rudolph+reindeer','christmas+lights','grinch','christmas+dinner','elfs','mrs+claus','presents','frosty+the+snowman','krampus','fireplace', 'christmas+stocking','candy+cane','scrooge','belsnickel','christmas+movies','christmas+music','ded+moroz']
-var words = ['Christmas','Navidad','Santa','Christmas Tree','Rudolph Reindeer','Christmas Lights','Grinch','Christmas Dinner','Elfs','Mrs Claus','Presents','Frosty The Snowman','Krampus','Fireplace', 'Christmas Stocking','Candy Cane','Scrooge','Belsnickel','Christmas Movies','Christmas Music','Ded Moroz']
-
-var queryURL = "http://api.giphy.com/v1/gifs/search?q="+topics[4]+"&api_key=9RsoHb5hiRrXeoIELy3wuORx8T83KmFk&limit=10"
-
 $(document).ready(function(){
-for (var i = 0; i < words.length; i++){
-    $("#buttons").append("<button>"+words[i]+"</button>")
-}
-$.ajax({
+
+    var audio = new Audio(['../images/christmas.mp4','../images/white_christmas.mp4','../images/let_it_snow.mp4','../images/holly_jolly.mp4','../images/navidad.mp4']);
+    audio = Math.floor(Math.random() * audio.length) + audio;
+
+var theGifs = ['Christmas','Navidad','Santa','Christmas Tree','Rudolph Reindeer','Christmas Lights','Grinch','Christmas Dinner','Elfs','Mrs Claus','Presents','Frosty The Snowman','Krampus','Fireplace', 'Christmas Stockings','Candy Cane','Scrooge','Belsnickel','Christmas Movies','Christmas Music','Ded Moroz'];
+
+function display() {
+
+  var newGif = $(this).attr("data-name");
+  var queryURL = "http://api.giphy.com/v1/gifs/search?q="+newGif+"&api_key=9RsoHb5hiRrXeoIELy3wuORx8T83KmFk&limit=10";
+
+  $.ajax({
     url: queryURL,
     method: "GET"
   }).then(function(response) {
-    for (var i = 0; i < 10; i++){
-        $("#gifs").append("<img src='"+response.data[i].images.fixed_height.url+"'>")
-    console.log(response.data[i].images);
-    }
+
+  $(".gif").empty();
+
+for (var i = 0; i < queryURL.length; i++){
+    var gifDiv = $("<div class='gif'>");
+    var rating = response.data[i].rating;
+    var pOne = $("<p>").text("Rating: "+rating);
+    gifDiv.append(pOne);
+    let gifURL = response.data[i].images.fixed_height_still.url;
+    let gif = $("<img>").attr("src", gifURL);
+    gifDiv.append(gif);
+    // document.getElementsByTagName('<img>').onclick = function(){
+    //   $("<img>").empty();
+    //   let gifURL = response.data.gifs.fixed_height.url;
+    //   let gif = $("<img>").attr("src", gifURL);
+    //   gifDiv.append(gif)
+    // }
+    // Putting the entire newGif above the previous theGifs
+    $("#theGifs").prepend(gifDiv);
+}
+gif.getElementsByTagName('<img>').onclick = function(){
+console.log("you clicked me!")
+let gifURL = response.data[i].images.fixed_height.url;
+$("<img>").attr("src",gifURL)
+}
   });
+
+}
+
+function makeButton() {
+  $("#btns").empty();
+
+  for (var i = 0; i < theGifs.length; i++) {
+    var adder = $("<button>");
+    adder.addClass("christmasBtn");
+    adder.attr("data-name", theGifs[i]);
+    adder.text(theGifs[i]);
+    $("#btns").append(adder);
+  }
+}
+
+$("#loadGif").on("click", function(event) {
+  event.preventDefault();
+  var newGif = $("#gifPlaceholder").val().trim();
+  theGifs.push(newGif);
+  makeButton();
+});
+
+$(document).on("click", ".christmasBtn", display);
+makeButton();
 })
-  

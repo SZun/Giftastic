60c7b503ed60c777e6b9305e705f34180edbd252
Sam G. Zun <zunsamuel97@gmail.com>
2018-04-21 17:36:11 -0500

    commit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,14 @@
 $(document).ready(function(){
 
-    var audio = new Audio(['../images/christmas.mp4','../images/white_christmas.mp4','../images/let_it_snow.mp4','../images/holly_jolly.mp4','../images/navidad.mp4']);
-    audio = Math.floor(Math.random() * audio.length) + audio;
+    var audio = ['../Giftastic/assets/images/christmas.mp4','../Giftastic/assets/images/white_christmas.mp4','../Giftastic/assets/images/let_it_snow.mp4','../Giftastic/assets/images/holly_jolly.mp4','../Giftastic/assets/images/navidad.mp4']
+    audio = audio[Math.floor(Math.random() * audio.length)]
+    audio = new Audio(audio)
+    audio.play();
 
 var theGifs = ['Christmas','Navidad','Santa','Christmas Tree','Rudolph Reindeer','Christmas Lights','Grinch','Christmas Dinner','Elfs','Mrs Claus','Presents','Frosty The Snowman','Krampus','Fireplace', 'Christmas Stockings','Candy Cane','Scrooge','Belsnickel','Christmas Movies','Christmas Music','Ded Moroz'];
 
 function display() {
-
+  
   var newGif = $(this).attr("data-name");
   var queryURL = "http://api.giphy.com/v1/gifs/search?q="+newGif+"&api_key=9RsoHb5hiRrXeoIELy3wuORx8T83KmFk&limit=10";
 
@@ -18,7 +20,7 @@ function display() {
   $(".gif").empty();
 
 for (var i = 0; i < queryURL.length; i++){
-    var gifDiv = $("<div class='gif'>");
+    var gifDiv = $("<div class='gif'>").css('float','left');
     var rating = response.data[i].rating;
     var pOne = $("<p>").text("Rating: "+rating);
     gifDiv.append(pOne);
@@ -61,6 +63,8 @@ $("#loadGif").on("click", function(event) {
   theGifs.push(newGif);
   makeButton();
 });
+
+// $("<input>").html('input:none')
 
 $(document).on("click", ".christmasBtn", display);
 makeButton();

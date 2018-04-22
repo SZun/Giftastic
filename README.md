5dacc0603316fea2f116db5385f348c26f7c9912
Sam G. Zun <zunsamuel97@gmail.com>
2018-04-22 07:07:12 -0500

    commit

--- README.md
+++ README.md
@@ -4,3 +4,13 @@
 <!-- Spacing of Gif/Rating concerning the search form group [] -->
 <!-- Gif moving from still to animate and back based on click -->
 <!-- Output 10 gifs that are != 'r' && !='pg-13' [] -->
+
+<!-- Bounus Stuff -->
+<!-- 
+    - Responsive [x]
+    - Add 10 more gifs []
+    - Add more data to the gifs (more than the rating) []
+    - 1 click button for downloading the gifs []
+    - Integrate additional API []
+    - Allows users to add gifs htey like to a foavoites secition []
+ -->
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,27 +3,31 @@ $(document).ready(function(){
     var audio = ['../Giftastic/assets/images/christmas.mp4','../Giftastic/assets/images/white_christmas.mp4','../Giftastic/assets/images/let_it_snow.mp4','../Giftastic/assets/images/holly_jolly.mp4','../Giftastic/assets/images/navidad.mp4','../Giftastic/assets/images/santa.mp4']
     audio = audio[Math.floor(Math.random() * audio.length)]
     audio = new Audio(audio)
-    // audio.play();
+    audio.play();
+    var limit = 10
 
 var theGifs = ['Christmas','Navidad','Santa','Christmas Tree','Rudolph Reindeer','Christmas Lights','Grinch','Christmas Dinner','Elfs','Mrs Claus','Presents','Frosty The Snowman','Krampus','Fireplace', 'Christmas Stockings','Candy Cane','Scrooge','Belsnickel','Christmas Movies','Christmas Music','Ded Moroz'];
 
 function display() {
-  
+  var apiKey = '9RsoHb5hiRrXeoIELy3wuORx8T83KmFk';
   var newGif = $(this).attr("data-name");
-  var queryURL = "http://api.giphy.com/v1/gifs/search?q="+newGif+"&api_key=9RsoHb5hiRrXeoIELy3wuORx8T83KmFk&limit=10";
+  var queryURL = "http://api.giphy.com/v1/gifs/search?q="+newGif+"&api_key="+apiKey+"&limit=10"+limit+"" ;
 
   $.ajax({
     url: queryURL,
     method: "GET"
   }).then(function(response) {
-
   $(".gif").empty();
-for (var i = 0; i < 10; i++){
+for (var i = 0; i < limit; i++){
    if(response.data[i].rating != 'r' && response.data[i].rating != 'pg-13'){
     var gifDiv = $("<div class='gif'>").css('float','left');
     var rating = response.data[i].rating;
     var ratingPElement = $("<p>").text("Rating: "+rating);
     gifDiv.append(ratingPElement);
+    
+    var source = response.data[i].source_tld;
+    var sourceElement = $("<div>").html("<a href="+"https://"+source+" target='_blank'>Source Link!!</a>");
+    gifDiv.append(sourceElement);
     let gifURL = response.data[i].images.fixed_height_still.url;
     var gif = $("<img>").attr("src", gifURL).addClass('imStill');
     gifDiv.append(gif);
@@ -42,7 +46,7 @@ for (var i = 0; i < 10; i++){
             $(this).removeClass("imAnimated");
             $(this).addClass("imStill");
             var gifURL = response.data[1].images.fixed_height_still.url;
-            $(this).attr("src",gifURL)
+            $(this).attr("src", gifURL)
           }
       }  
     })
@@ -70,8 +74,6 @@ $("#loadGif").on("click", function(event) {
   theGifs.push(newGif);
   makeButton();
 });
-
-// $("<input>").html('input:none')
 
 $(document).on("click", ".christmasBtn", display);
 makeButton();

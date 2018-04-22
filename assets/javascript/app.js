01582cc6505693442100981e93cdd0367d3b9608
Sam G. Zun <zunsamuel97@gmail.com>
2018-04-22 03:10:27 -0500

    commit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     var audio = ['../Giftastic/assets/images/christmas.mp4','../Giftastic/assets/images/white_christmas.mp4','../Giftastic/assets/images/let_it_snow.mp4','../Giftastic/assets/images/holly_jolly.mp4','../Giftastic/assets/images/navidad.mp4']
     audio = audio[Math.floor(Math.random() * audio.length)]
     audio = new Audio(audio)
-    audio.play();
+    // audio.play();
 
 var theGifs = ['Christmas','Navidad','Santa','Christmas Tree','Rudolph Reindeer','Christmas Lights','Grinch','Christmas Dinner','Elfs','Mrs Claus','Presents','Frosty The Snowman','Krampus','Fireplace', 'Christmas Stockings','Candy Cane','Scrooge','Belsnickel','Christmas Movies','Christmas Music','Ded Moroz'];
 
@@ -25,22 +25,31 @@ for (var i = 0; i < 10; i++){
     var ratingPElement = $("<p>").text("Rating: "+rating);
     gifDiv.append(ratingPElement);
     let gifURL = response.data[i].images.fixed_height_still.url;
-    let gif = $("<img>").attr("src", gifURL).addClass('imStill');
+    var gif = $("<img>").attr("src", gifURL).addClass('imStill');
     gifDiv.append(gif);
     $("#gifSpace").prepend(gifDiv);
-    $(".imStill").on("click", function(){
-      for (var i = 0; i < 10; i++){ 
-        if ($(this).hasClass('imStill')){
-            console.log(response.data[i].images.fixed_height.url)
-            // var movingGif = response.data[i].images.fixed_height.url;
-            // $(this).attr("src", movingGif)
-            $(this).removeClass("imStill").addClass("imAnimated")
+    $(".imStill" || ".imAnimated").on("click", function(){
+      // for (var i = 0; i < 10; i++){ 
+      //   var movingGif = response.data[i].images.fixed_height.url;
+      //   var gifURL = response.data[i].images.fixed_height_still.url;
+        if ($(this).hasClass("imStill")){
+          for (var i = 0; i < 10; i++){ 
+            // console.log(response.data[i].images.fixed_height.url)
+            var movingGif = response.data[i].images.fixed_height.url;
+            $(this).attr("src", movingGif)
+            $(this).removeClass("imStill");
+            $(this).addClass("imAnimated");
+            }
           }
-          // else{
-          //   $(this).attr("src",gifURL)
-          //   $(this).removeClass("imAnimated").addClass("imStill")
-          // }
-      }  
+          else if($(this).hasClass("imAnimated")){
+            for (var i = 0; i < 10; i++){ 
+            $(this).removeClass("imAnimated");
+            $(this).addClass("imStill");
+            var gifURL = response.data[i].images.fixed_height_still.url;
+            $(this).attr("src",gifURL)
+            }
+          }
+      // }  
     })
   }
 }
